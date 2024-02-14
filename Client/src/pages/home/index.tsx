673be3610ffd4a55d53ad2c89f89c19f96eebb3c
Kahraman <80291704+kahramanX@users.noreply.github.com>
2024-02-14 22:52:01 +0300

    newsletter section added

--- Client/src/pages/home/index.tsx
+++ Client/src/pages/home/index.tsx
@@ -119,6 +119,7 @@ const HomePage = (): JSX.Element => {
                         </button>
                     </div>
                 </section>
+
                 <section className="my-20">
                     <Swiper
                         modules={[]}
@@ -145,6 +146,36 @@ const HomePage = (): JSX.Element => {
                             );
                         })}
                     </Swiper>
+                </section>
+
+                <section>
+                    <div className="layered-box-shadow bg-blue flex flex-col items-center p-4 gap-4">
+                        <div className="text-2xl lg:text-3xl font-bold">
+                            Get 10% Discount
+                        </div>
+                        <div className="text-sm italic">
+                            Subscribe to out newsletter to get the latest offers
+                            & promotion
+                        </div>
+                        <div className="flex flex-col gap-4 w-72">
+                            <input
+                                placeholder="First Name *"
+                                className="input text-xs"
+                                type="text"
+                            />
+                            <button className="button bg-grey1 text-xs py-2">
+                                Submit
+                            </button>
+                            <div className="text-xs text-center">
+                                Proceeding I confirm that I have read and
+                                understand the information on the use of my
+                                personal data explained in the Privacy Policy,
+                                and I agree to receive customized commercial
+                                communications from Pull&Bear by email and other
+                                means.
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </>
