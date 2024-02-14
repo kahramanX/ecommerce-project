37d7d7832f46efed8047e73c7cec18792e8b8c15
Kahraman <80291704+kahramanX@users.noreply.github.com>
2024-02-14 22:38:27 +0300

    promotion section added

--- Client/src/pages/home/index.tsx
+++ Client/src/pages/home/index.tsx
@@ -15,7 +15,7 @@ const HomePage = (): JSX.Element => {
         <>
             <ReactHelmet titleName="Home" description="Home Page" />
             <main className="px-2 md:px-4 lg:px-10">
-                <div className="gap-8 grid grid-cols-1 lg:grid-cols-1-2-1">
+                <section className="gap-8 grid grid-cols-1 lg:grid-cols-1-2-1">
                     <div className="layered-box-shadow hidden lg:block lg:max-h-[40rem] bg-blue">
                         <img
                             src={"https://placehold.co/400"}
@@ -76,33 +76,76 @@ const HomePage = (): JSX.Element => {
                             className="h-full w-full object-cover opacity-50"
                         />
                     </div>
-                </div>
-                <Swiper
-                    className="my-20"
-                    modules={[]}
-                    slidesPerView={2}
-                    spaceBetween={16}
-                    wrapperClass="py-2"
-                    breakpoints={{
-                        768: {
-                            slidesPerView: 3,
-                        },
-                        1024: {
-                            slidesPerView: 4,
-                            spaceBetween: 32,
-                        },
-                    }}
-                >
-                    {[1, 2, 3].map(() => {
-                        return (
-                            <>
-                                <SwiperSlide>
-                                    <Product />
-                                </SwiperSlide>
-                            </>
-                        );
-                    })}
-                </Swiper>
+                </section>
+                <section className="my-20">
+                    <Swiper
+                        modules={[]}
+                        slidesPerView={2}
+                        spaceBetween={16}
+                        wrapperClass="py-2"
+                        breakpoints={{
+                            768: {
+                                slidesPerView: 3,
+                            },
+                            1024: {
+                                slidesPerView: 4,
+                                spaceBetween: 32,
+                            },
+                        }}
+                    >
+                        {[1, 2, 3, 4].map(() => {
+                            return (
+                                <>
+                                    <SwiperSlide>
+                                        <Product />
+                                    </SwiperSlide>
+                                </>
+                            );
+                        })}
+                    </Swiper>
+                </section>
+
+                <section>
+                    <div className="layered-box-shadow bg-green flex flex-col items-center p-4 gap-4">
+                        <div className="text-2xl lg:text-3xl font-bold">
+                            PROMOTIONS
+                        </div>
+                        <div className="text-4xl lg:text-6xl font-bold">
+                            UP TO -40%
+                        </div>
+                        <div className="italic">On Selected Items</div>
+                        <button className="button text-xs bg-grey1 px-2 py-2">
+                            Shop All
+                        </button>
+                    </div>
+                </section>
+                <section className="my-20">
+                    <Swiper
+                        modules={[]}
+                        slidesPerView={2}
+                        spaceBetween={16}
+                        wrapperClass="py-2"
+                        breakpoints={{
+                            768: {
+                                slidesPerView: 3,
+                            },
+                            1024: {
+                                slidesPerView: 4,
+                                spaceBetween: 32,
+                            },
+                        }}
+                    >
+                        {[1, 2, 3, 4].map(() => {
+                            return (
+                                <>
+                                    <SwiperSlide>
+                                        <Product />
+                                    </SwiperSlide>
+                                </>
+                            );
+                        })}
+                    </Swiper>
+                </section>
             </main>
         </>
     );
