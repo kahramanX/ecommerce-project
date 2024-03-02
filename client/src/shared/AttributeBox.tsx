f3df11a216764edb1236af26e07a85da56a03b44
Kahraman <80291704+kahramanX@users.noreply.github.com>
2024-03-02 22:50:02 +0300

    attribute box bug fix

--- client/src/shared/AttributeBox.tsx
+++ client/src/shared/AttributeBox.tsx
@@ -11,75 +11,71 @@ const AttributeBox: React.FC<Props> = () => {
     const [isAttributeClose, setIsAttributeClose] = useState<boolean>(false);
 
     return (
-        <div>
-            <div className="layered-box-shadow bg-violet p-2">
-                <div className="flex gap-4">
+        <div className="layered-box-shadow bg-violet p-2 mb-4">
+            <div className="flex gap-4">
+                <Button
+                    sizes="text-sm w-full"
+                    colors="bg-violet"
+                    spacings="py-2 gap-2"
+                    extraClass="flex justify-center items-center"
+                    buttonContent={
+                        <>
+                            <Text
+                                fontColor="text-black"
+                                fontSize="text-base"
+                                fontWeight="font-semibold"
+                                textContent={<>Sorting</>}
+                            />
+                        </>
+                    }
+                />
+                <Button
+                    sizes="text-sm"
+                    colors="bg-grey1"
+                    spacings="py-2 px-2 lg:px-4"
+                    extraClass="flex justify-center items-center"
+                    buttonContent={
+                        <>
+                            <Icon name="add" extraClass="text-xs" />
+                            {/* <Icon
+                        name="remove"
+                        extraClass="text-xs"
+                    /> */}
+                        </>
+                    }
+                    onClick={() => setIsAttributeClose((current) => !current)}
+                />
+            </div>
+
+            {isAttributeClose && (
+                <>
+                    <div className="grid grid-cols-4 gap-4 mt-4">
+                        {Array.from({ length: 10 }).map(() => (
+                            <>
+                                <div className="border border-grey3 bg-grey1 flex items-center justify-center cursor-pointer">
+                                    S
+                                </div>
+                            </>
+                        ))}
+                    </div>
                     <Button
                         sizes="text-sm w-full"
-                        colors="bg-violet"
+                        colors="bg-grey1"
                         spacings="py-2 gap-2"
-                        extraClass="flex justify-center items-center"
+                        extraClass="flex justify-center items-center mt-4"
                         buttonContent={
                             <>
                                 <Text
                                     fontColor="text-black"
                                     fontSize="text-base"
                                     fontWeight="font-semibold"
-                                    textContent={<>Sorting</>}
+                                    textContent={<>Reset</>}
                                 />
                             </>
                         }
                     />
-                    <Button
-                        sizes="text-sm"
-                        colors="bg-grey1"
-                        spacings="py-2 px-2 lg:px-4"
-                        extraClass="flex justify-center items-center"
-                        buttonContent={
-                            <>
-                                <Icon name="add" extraClass="text-xs" />
-                                {/* <Icon
-                        name="remove"
-                        extraClass="text-xs"
-                    /> */}
-                            </>
-                        }
-                        onClick={() =>
-                            setIsAttributeClose((current) => !current)
-                        }
-                    />
-                </div>
-
-                {isAttributeClose && (
-                    <>
-                        <div className="grid grid-cols-4 gap-4 mt-4">
-                            {Array.from({ length: 10 }).map(() => (
-                                <>
-                                    <div className="border border-grey3 bg-grey1 flex items-center justify-center cursor-pointer">
-                                        S
-                                    </div>
-                                </>
-                            ))}
-                        </div>
-                        <Button
-                            sizes="text-sm w-full"
-                            colors="bg-grey1"
-                            spacings="py-2 gap-2"
-                            extraClass="flex justify-center items-center mt-4"
-                            buttonContent={
-                                <>
-                                    <Text
-                                        fontColor="text-black"
-                                        fontSize="text-base"
-                                        fontWeight="font-semibold"
-                                        textContent={<>Reset</>}
-                                    />
-                                </>
-                            }
-                        />
-                    </>
-                )}
-            </div>
+                </>
+            )}
         </div>
     );
 };
