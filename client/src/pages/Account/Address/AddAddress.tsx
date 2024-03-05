fd2a0becb0d7a307f1b2550ff540830b73405f44
Kahraman <80291704+kahramanX@users.noreply.github.com>
2024-03-05 22:52:24 +0300

    add address page added

--- client/src/pages/Account/Address/AddAddress.tsx
+++ client/src/pages/Account/Address/AddAddress.tsx
@@ -11,30 +11,75 @@ const AddAddress: React.FC<Props> = () => {
         <div className="p-2 lg:p-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
             <Input
                 type={"text"}
-                placeholder={"Current Password"}
+                placeholder={"Address Name"}
                 parentClassName="w-full"
                 inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"Name"}
+                parentClassName="w-full"
+                inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"Surname"}
+                parentClassName="w-full"
+                inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"Phone Number"}
+                parentClassName="w-full"
+                inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"Country"}
+                parentClassName="w-full"
+                inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"County"}
+                parentClassName="w-full"
+                inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"Neighbourhood"}
+                parentClassName="w-full"
+                inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"Postal Code"}
+                parentClassName="w-full"
+                inputClassName="w-full"
+            />
+            <Input
+                type={"text"}
+                placeholder={"Address Detail"}
+                parentClassName="w-full lg:col-span-2"
+                inputClassName="w-full h-40"
             />
             <div></div>
-            <Input
-                type={"text"}
-                placeholder={"New Password"}
-                parentClassName="w-full"
-                inputClassName="w-full"
-            />
-            <Input
-                type={"text"}
-                placeholder={"New Password Again"}
-                parentClassName="w-full"
-                inputClassName="w-full"
-            />
-            <Button
-                sizes="w-full"
-                colors="bg-grey1"
-                spacings="py-2"
-                extraClass="lg:col-span-2"
-                buttonContent="Update Password"
-            />
+            <div className="flex lg:col-span-2 gap-4">
+                <Button
+                    sizes="w-full"
+                    colors="bg-grey1"
+                    spacings="py-2"
+                    extraClass="lg:col-span-1"
+                    buttonContent="Add Address"
+                />
+                <Button
+                    sizes="w-full"
+                    colors="bg-grey1"
+                    spacings="py-2"
+                    extraClass="lg:col-span-1"
+                    buttonContent="Cancel"
+                />
+            </div>
         </div>
     );
 };
