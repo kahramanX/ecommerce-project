b4ecdb20f9dcda647e8224962734eaf8129fc0c0
Kahraman <80291704+kahramanX@users.noreply.github.com>
2024-03-07 00:15:57 +0300

    Update MessageDetail.tsx

--- client/src/pages/Account/Message/MessageDetail.tsx
+++ client/src/pages/Account/Message/MessageDetail.tsx
@@ -27,14 +27,61 @@ const MessageDetail: React.FC<Props> = ({ setMessageViewType }) => {
                 }
                 onClick={() => setMessageViewType("message_list")}
             />
-            <div className=" bg-grey1 grid grid-cols-2 lg:grid-cols-2 gap-2 p-2 my-4">
-                <div>Date:</div> <div>26.01.2024 20:21</div>
-                <div>Title:</div> <div>Ürün beden bilgisi</div>
-                <div>Content:</div> <div>Content Detail</div>
+            <div className=" border-b bg-grey1 grid grid-cols-2 lg:grid-cols-2 gap-2 p-2 my-4">
+                <div className="font-bold">Date:</div>
+                <div>26.01.2024 20:21</div>
+                <div className="font-bold">Title:</div>
+                <div>Ürün beden bilgisi</div>
+                <div className="font-bold">Content:</div>
+                <div>
+                    Content Detail Content Detail Content Detail Content Detail
+                    Content Detail Content Detail Content Detail Content Detail
+                    Content Detail Content Detail Content Detail Content Detail
+                </div>
             </div>
-            <div className="border p-2">
-                <div>SOL</div>
-                <div>SAĞ</div>
+            <div className="p-2 flex flex-col">
+                <div className="border mb-4 p-2 lg:w-1/2 bg-violet">
+                    <div className="flex items-center mb-2">
+                        <Icon name="verified_user" extraClass="text-xs" />
+                        <span className="text-xs font-medium">
+                            Müşteri Hizmetleri
+                        </span>
+                        <span className="text-xs text-grey4 ms-2">
+                            26.01.2024 20:21
+                        </span>
+                    </div>
+                    <div className="text-md">
+                        Merhaba, ben müşteri hizmetleri
+                    </div>
+                </div>
+
+                <div className="border mb-4 p-2 lg:w-1/2 bg-grey2 ">
+                    <div className="flex items-center mb-2">
+                        <Icon name="verified_user" extraClass="text-xs" />
+                        <span className="text-xs font-medium">
+                            Alagar Kahraman
+                        </span>
+                        <span className="text-xs text-grey4 ms-2">
+                            26.01.2024 20:21
+                        </span>
+                    </div>
+                    <div className="text-md">Merhaba, ben müşteri</div>
+                </div>
+
+                <div className="border mb-4 p-2 lg:w-1/2 bg-violet">
+                    <div className="flex items-center mb-2">
+                        <Icon name="verified_user" extraClass="text-xs" />
+                        <span className="text-xs font-medium">
+                            Müşteri Hizmetleri
+                        </span>
+                        <span className="text-xs text-grey4 ms-2">
+                            26.01.2024 20:21
+                        </span>
+                    </div>
+                    <div className="text-md">
+                        Merhaba, ben müşteri hizmetleri
+                    </div>
+                </div>
             </div>
         </div>
     );
